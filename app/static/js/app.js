e75d1e6b4cf536bcf8b557b535d3691dc15960a8
DmitriBrach <dmitribrach@gmail.com>
2015-04-27 15:39:33 +0300

    Angular controllers, factories, services and directives

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,89 @@
 // Declare app level module which depends on filters, and services
-angular.module('angular_blog', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home/home.html', 
-        controller: 'HomeController'})
-      .otherwise({redirectTo: '/'});
+window.Blog = angular.module('angular_blog', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date', 'restangular', 'LocalStorageModule'])
+    run(function ($location, Restangular, AuthService) {
+        Restangular.setFullRequestInterceptor(function (element, operation, route, url, headers, params, httpConfig) {
+            headers['Authorization'] = 'Basic' + AuthService.getterToken();
+            return {
+                headers: headers
+            };
+        });
+
+        Restangular.setErrorInterceptor(function (response, deferred, responseHandler) {
+            if(response.config.bypassInterseptor) {
+                return true;
+            } else {
+                switch (response.status) {
+                    case 401:
+                        AuthService.logout();
+                        $location.path('/sessions/create');
+                        break;
+                    default:
+                        throw new Error('No handler for status code ' + response.status);
+                }
+                return false;
+            }
+        });
+    })
+
+    .config(['$routeProvider', function ($routeProvider, RestangularProvider) {
+            RestangularProvider.setBaseUrl('http://localhost:5000/api/v1');
+            var partialsDir = '../partials';
+
+            var redirectIfAuthenticated = function (route) {
+                return function ($location, $q, AuthService) {
+                    var deferred = $q.defer();
+                    if (AuthService.isAuthenticated()) {
+                        deferred.reject();
+                        $location.path(route)
+                    } else {
+                        deferred.resolve();
+                    }
+                    return deferred.promise;
+                }
+            };
+
+            var redirectIfNotAuthenticated = function (route) {
+                return function ($location, $q, AuthService) {
+                    var deferred = $q.defer();
+
+                    if (! AuthService.isAuthenticated()) {
+                        deferred.reject()
+                        $location.path(route);
+                    } else {
+                        deferred.resolve()
+                    }
+
+                    return deferred.promise;
+                }
+            };
+
+
+
+        $routeProvider
+        .when('/', {
+            controller: 'HomeDetailCtrl',
+            templateUrl: partialsDir + '/home/detail.html'
+        })
+        .when('/sessions/create', {
+            controller: 'SessionCreateCtrl',
+            templateUrl: partialsDir + '/session/create.html',
+            resolve: {
+                redirectIfAuthenticated: redirectIfAuthenticated('/posts/create')
+            }
+        })
+        .when('/sessions/destroy', {
+            controller: 'SessionDestroyCtrl',
+            templateUrl: partialsDir + '/session/destroy.html'
+        })
+        .when('/users/create', {
+            controller: 'UserCreateCtrl',
+            templateUrl: partialsDir + '/user/create.html'
+        })
+        .when('/posts/create', {
+            controller: 'PostCreateCtrl',
+            templateUrl: partialsDir + '/post/create.html',
+            resolve: {
+                redirectIfNotAuthenticated: redirectIfNotAuthenticated('/sessions/create')
+            }
+        });
   }]);
